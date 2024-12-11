9456a5e1a3a8ddaedf54ce6e18c36ed838d2225b
Suryakant Chandrakar <785surya@gmail.com>
2024-12-11 22:18:57 +0530

    abcd

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 app.get('/', async (req, res) => {
